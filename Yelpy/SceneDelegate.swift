8f8a28fdbb9ca1ed466641f261be7087b825ba7f
Ngan Nguyen <hieungan5b@gmail.com>
2022-04-21 00:44:40 -0500

    auth

--- Yelpy/SceneDelegate.swift
+++ Yelpy/SceneDelegate.swift
@@ -14,10 +14,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        
+    // Add event listener for when user logs in
+        NotificationCenter.default.addObserver(forName: Notification.Name("login"), object:nil, queue: OperationQueue.main) {
+            (Notification) in
+            print("Log in notification received")
+            self.login()
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func login() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // view controller currently being set in Storyboard as default will be overriden
+        window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "TabBar")
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- Yelpy/Storyboards/LoginViewController.swift
+++ Yelpy/Storyboards/LoginViewController.swift
@@ -19,19 +19,93 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    // SIGN UP
     @IBAction func onSignUp(_ sender: Any) {
+        print("hey")
         if usernameAndPasswordNotEmpty() {
+            print("not empty")
             // initialize a user object
             let newUser = PFUser()
             
+            // set user properties
+            newUser.username = usernameTextField.text
+            newUser.password = passwordTextField.text
             
+            // call sign up function on the object
+            newUser.signUpInBackground {
+                (success: Bool, error: Error?) in
+                if let error = error {
+                    print(error.localizedDescription)
+                    self.displaySignupError(error:error)
+                } else {
+                    print("User \(newUser.username!) Registered successfully")
+                    NotificationCenter.default.post(name: NSNotification.Name("login"), object: nil)
+                }
+            }
+            
+        }
+    }
+    // LOG IN
+    @IBAction func onLogin(_ sender: Any) {
+        // Check text field inputs
+        if usernameAndPasswordNotEmpty() {
+            let username = usernameTextField.text ?? ""
+            let password = passwordTextField.text ?? ""
+            PFUser.logInWithUsername(inBackground: username, password: password) {
+                (user: PFUser?, error: Error?) in
+                if let error = error {
+                    print("User log in failed: \(error.localizedDescription)")
+                    self.displayLoginError(error: error)
+                } else {
+                    print("User \(username) logged in successfully")
+                    // display view controller that needs to be shown after successful login
+                    NotificationCenter.default.post(name: NSNotification.Name("login"), object: nil)
+                }
+            }
         }
     }
     
     /*------ Handle text field inputs  ------*/
     func usernameAndPasswordNotEmpty() -> Bool {
-        return usernameTextField.text!.isEmpty || passwordTextField.text!.isEmpty
+        return !usernameTextField.text!.isEmpty && !passwordTextField.text!.isEmpty
     }
+    
+    /*------ Alert Controllers ------*/
+    // Text fields are empty alert controller
+    func displayError() {
+        let title = "Error"
+        let message = "Username and password field cannot be empty"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
+    // Sign up error alert controller
+    func displaySignupError(error: Error) {
+        let title = "Sign up error"
+        let message = "Oops! Something went wrong while signing up: \(error.localizedDescription)"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title:"OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated:true)
+    }
+    
+    // Login error alert controller
+    func displayLoginError(error: Error) {
+        let title = "Login Error"
+        let message = "Oops! Something went wrong while logging in: \(error.localizedDescription)"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        // create an OK action
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
+        present(alertController, animated: true)
+    }
+    
 
 }
